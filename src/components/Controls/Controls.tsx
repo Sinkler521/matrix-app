ab3b151cf9e57de68ce3abc5fb47a115a63c65ed
Sinkler521 <Sinkler521@gmail.com>
2025-12-25 19:04:53 +0200

    controls UI changes

--- src/components/Controls/Controls.tsx
+++ src/components/Controls/Controls.tsx
@@ -5,8 +5,13 @@ export const Controls = () => {
     return (
         <>
           <div className="controls">
-            <div className="controls-element">a</div>
-            <div className="controls-element justify-center">a</div>
+            <div className="controls-element">
+                <input className="controls-input" type="number" min={1} max={100} placeholder="M (rows)"/>
+                <input className="controls-input" type="number" min={1} max={100} placeholder="N (cols)"/>
+            </div>
+            <div className="controls-element justify-center">
+                <button className="controls-button">Generate</button>
+            </div>
           </div>
         </>
     )
