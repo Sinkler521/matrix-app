51e2ebf930a56bf631bb67feae62ff3f850bbf4c
Sinkler521 <Sinkler521@gmail.com>
2025-12-25 18:48:53 +0200

    some skeletons added, especially controls

--- src/components/Controls/Controls.tsx
+++ src/components/Controls/Controls.tsx
@@ -4,7 +4,10 @@ export const Controls = () => {
 
     return (
         <>
-          <div className="controls"></div>
+          <div className="controls">
+            <div className="controls-element">a</div>
+            <div className="controls-element justify-center">a</div>
+          </div>
         </>
     )
 }
--- src/types.ts
+++ src/types.ts
@@ -5,3 +5,12 @@ export type Cell = {
   id: CellId,
   amount: CellValue
 }
+
+export type Matrix = Cell[][];
+
+export type Settings = {
+  // Sinkler521 matrix M and N limits 1-100, x values are calculated
+  M: number; // Sinkler521: rows
+  N: number; // Sinkler521: cols
+  X: number; // Sinkler521: how many nearest cells to highlight
+};
